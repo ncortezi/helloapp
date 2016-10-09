b3479bbeae0e9ed455e4719cdb44d7c4e3835142
Nick Cortezi <ncortezi92@gmail.com>
2016-10-09 18:11:36 -0400

    Change lang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "¡Hola, Mundo!"
+  	render html: "¡HIT"
   end
   def goodbye
   	render html: "No one here but us chickens"
